d82f8ba4355e87949025c993e4fc7ae7c508ef46
JuliannyAraujo <rm99554@fiap.com.br>
2023-11-23 20:09:46 -0300

    Adicionando imagem

--- saudeparatodos/src/pages/sobre.js
+++ saudeparatodos/src/pages/sobre.js
@@ -7,27 +7,29 @@ const Sobre = () => (
     <Navigation />
     <div className="sobre-container">
       <div className="sobre-titulo">
-        <h1 style={{ fontSize: '32px' }}>Apresentação</h1>
+        <h1 style={{ fontSize: '32px' }}>Apresentação da Missão, Visão e Valores</h1>
       </div>
       <div className="sobre-conteudo">
         <div className="sobre-item">
           <h2 style={{ fontSize: '24px' }}>Missão</h2>
           <p style={{ fontSize: '18px' }}>
             Inovar a capacidade de um atendimento acessível para comunidade e região com o projeto para todos ter acesso e atendimento garantido.
+            <img src="/atendimento.png" alt="Atendimento" style={{ marginTop: '10px', width: '100px', height: 'auto', display: 'block' }} />
           </p>
-          {/* Adicione a imagem abaixo do texto Missão */}
-          <img src="/atendimento.png" alt="Adicional" style={{ marginTop: '10px', width: '100px', height: 'auto' }} />
         </div>
         <div className="sobre-item">
           <h2 style={{ fontSize: '24px' }}>Visão</h2>
           <p style={{ fontSize: '18px' }}>
             Buscar a infraestrutura para melhorar cada vez mais e assim, pode se tornar a melhor clínica em valor acessível para comunidade/região.
+            <img src="/acessibilidade.png" alt="Acessibilidade" style={{ marginTop: '10px', width: '100px', height: 'auto', display: 'block' }} />
           </p>
         </div>
         <div className="sobre-item">
           <h2 style={{ fontSize: '24px' }}>Proposta de Valor</h2>
           <p style={{ fontSize: '18px' }}>
             Ter equipamentos de alta qualidade e tecnologia, assim tendo profissionais capacitados para cada área da saúde.
+            {/* Adicione a imagem abaixo do texto Proposta de Valor */}
+            <img src="/valor.png" alt="Proposta" style={{ marginTop: '18px', width: '100px', height: 'auto', display: 'block' }} />
           </p>
         </div>
       </div>
