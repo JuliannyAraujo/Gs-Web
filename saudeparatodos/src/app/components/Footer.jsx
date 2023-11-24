fb1b742d7febcf7cb2bb89756de94dd7dc6d9206
JuliannyAraujo <rm99554@fiap.com.br>
2023-11-23 22:00:36 -0300

    Colocando nome dos participantes

--- saudeparatodos/src/app/components/Footer.jsx
+++ saudeparatodos/src/app/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer>
       <hr/>
-      <p>criado por Saúde para todos&copy; Caio Cesar 99456   -   Julianny Araujo Pereira 99554   -  Jaqueline Martins 551744  -   Karen Vitória 99468   -  Matheus Matos 99792
+      <p>criado por Saúde para todos&copy; Caio Cesar 99456   -   Julianny Araujo Pereira 99554   -  Jaqueline Martins 551744  -   Karen Vitória 99468   -  Matheus Matos 99792 - Turma 1TDSPY
       </p>
     </footer>
   );
