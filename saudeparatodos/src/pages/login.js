b2676f7d320429e963c70d98d18a319b203a7455
JuliannyAraujo <rm99554@fiap.com.br>
2023-11-24 12:37:53 -0300

    Alteração layout pagina login

--- saudeparatodos/src/pages/login.js
+++ saudeparatodos/src/pages/login.js
@@ -1,7 +1,5 @@
-// pages/login.js
 import Link from 'next/link';
 import React from 'react';
-// import Navigation from './components/Navigation';
 
 export default function LoginPage() {
   const fazerLogin = () => {
@@ -9,21 +7,59 @@ export default function LoginPage() {
   };
 
   return (
-    <div>
+    <div className="login">
       <section id="content">
         <h2>Login</h2>
         <form id="login-form">
-          <label htmlFor="cpf">Digite seu cpf:</label>
-          <input type="cpf" id="cpf" name="cpf" required />
+          <div className="form-group">
+            <label htmlFor="cpf">Digite seu CPF:</label>
+            <input type="text" id="cpf" name="cpf" required />
+          </div>
 
-          <label htmlFor="senha">Senha:</label>
-          <input type="password" id="senha" name="senha" required />
+          <div className="form-group">
+            <label htmlFor="senha">Senha:</label>
+            <input type="password" id="senha" name="senha" required />
+          </div>
 
           <button type="button" onClick={fazerLogin}>Login</button>
         </form>
       </section>
 
       <p>Ainda não tem uma conta? <Link href="/criarConta">Crie uma conta</Link></p>
+
+      <style jsx>{`
+        .login {
+          background-color: #f0f0f0;
+          padding: 20px;
+        }
+
+        #content {
+          max-width: 600px;
+          margin: 0 auto;
+        }
+
+        .form-group {
+          margin-bottom: 15px;
+        }
+
+        label {
+          display: block;
+          margin-bottom: 5px;
+        }
+
+        input {
+          width: 100%;
+          padding: 8px;
+          box-sizing: border-box;
+        }
+
+        button {
+          background-color: #4caf50;
+          color: white;
+          padding: 10px;
+          cursor: pointer;
+        }
+      `}</style>
     </div>
-  )
+  );
 }
